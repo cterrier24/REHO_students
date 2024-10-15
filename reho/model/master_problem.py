274d66e7f37bf7a8b43c2383b0121a261926ecdb
mpesent <mirco.pesentibucella@epfl.ch>
2024-10-15 09:57:52 +0200

    Correction max PV

--- reho/model/master_problem.py
+++ reho/model/master_problem.py
@@ -332,7 +332,7 @@ class MasterProblem:
                 print("########################################")
                 print(h)
                 print("########################################")
-                raise Exception('Sub problem' + h + 'did not converge')
+                raise Exception('Sub problem ' + h + ' did not converge')
                 
 
         return df_Results, attr
@@ -789,7 +789,7 @@ class MasterProblem:
         if exitcode != 0:
             # It might be that the solution is optimal with unscaled infeasibilities. So we check if we really found a solution (via its cost value)
             if exitcode != 'solved?' or df_Results["df_Performance"]['Costs_op'][0] + df_Results["df_Performance"]['Costs_inv'][0] == 0:
-                raise Exception('Sub problem did not converge')
+                raise Exception('Sub problem '+h+' did not converge')
 
         return df_Results, attr
 
